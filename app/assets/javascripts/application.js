f59fdc9210a9a061a19c5ffaedd6b77d738883b0
Sebastian Gräßl <sebastian@validcode.me>
2016-12-12 12:57:02 +0100

    WIP #3551 - Add warning as help block of environment select

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -339,8 +339,22 @@ $(function() {
 });
 
 function check_for_unavailable_puppetclasses() {
-  if ($('#puppet_klasses #selected_classes .unavailable').size() > 0) {
-    notify('<span>' + __('Some Puppet classes are unavailable in the selected environment') + '</span>', 'warning')
+  var unavailable_classes = $('#puppet_klasses #selected_classes .unavailable');
+  var help_block = $('#puppet_klasses').closest('form')
+                     .find('#hostgroup_environment_id')
+                     .closest('.form-group').find('div+.help-block');
+
+  if (unavailable_classes.size() > 0) {
+    var warning = '<span id="puppetclasses_unavaliable_warning"><span class="pficon pficon-warning-triangle-o"></span> ' +
+                  Jed.sprintf(__('Some %s are unavailable in the selected environment'),
+                              '<a href="#puppet_klasses">Puppet classes</a>') + '</span>';
+    help_block.append(warning).find('a').on('click', function (event) {
+      event.preventDefault();
+      var tab = $(event.target).closest('form').find('.nav-tabs a[href="#puppet_klasses"]');
+      tab.tab('show')
+    })
+  } else {
+    help_block.find('#puppetclasses_unavaliable_warning').remove();
   }
 }
 
