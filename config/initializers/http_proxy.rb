13c7c6f393b0e733951598a6624cb942cba4b585
Sebastian Gräßl <mail@bastilian.me>
2017-07-04 10:16:22 +0200

    Refactor and add tests for http proxy feature

--- config/initializers/http_proxy.rb
+++
@@ -1,4 +0,0 @@
-if Setting::General.find_by_name('http_proxy')
-  require 'foreman/http_proxy/excon_connection_extension'
-  require 'foreman/http_proxy/net_http_extension'
-end
--- lib/foreman/http_proxy.rb
+++ lib/foreman/http_proxy.rb
@@ -1,13 +1,5 @@
 module Foreman
   module HTTPProxy
-    def logger
-      Foreman::Logging.logger('app')
-    end
-
-    def log_proxied_request(current_proxy, requested_host)
-      logger.info "Proxying request to #{requested_host} via #{current_proxy}"
-    end
-
     def http_proxy
       Setting::General.find_by_name('http_proxy').value
     end
@@ -16,16 +8,35 @@ module Foreman
       Setting::General.find_by_name('http_proxy_except_list').value
     end
 
-    def http_host_excepted?(host)
-      !http_proxy_except_list.include? host
+    def proxy_http_request?(current_proxy, request_host, schema)
+      !http_proxy.nil? && current_proxy.nil? && !request_host.nil? &&
+      schema != 'unix' &&
+      !http_host_excepted?(request_host) &&
+      !http_host_excepted_by_wildcard?(request_host)
     end
 
-    def proxy_http_request?(current_proxy, request_host, schema)
-      !http_proxy.nil? &&
-      current_proxy.nil? &&
-      !request_host.nil? &&
-      http_host_excepted?(request_host) &&
-      schema != 'unix'
+    private
+
+    def logger
+      Foreman::Logging.logger('app')
+    end
+
+    def log_proxied_request(current_proxy, requested_host)
+      logger.info "Proxying request to #{requested_host} via #{current_proxy}"
+    end
+
+    def http_host_excepted_by_wildcard?(host)
+      http_proxy_except_list.collect do |value|
+        return unless value.include? '*'
+        host.match(/#{value.gsub('*', '')}/)
+      end.any?
+    end
+
+    def http_host_excepted?(host)
+      http_proxy_except_list.include? host
     end
   end
 end
+
+require_dependency File.expand_path('../http_proxy/excon_connection_extension', __FILE__)
+require_dependency File.expand_path('../http_proxy/net_http_extension', __FILE__)
