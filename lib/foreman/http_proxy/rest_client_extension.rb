0fe54d2b7b9256197f23b797687ce571ea3201fb
Sebastian Gräßl <mail@bastilian.me>
2017-07-31 17:42:48 +0200

    Correct RestClient proxy_uri method

--- lib/foreman/http_proxy/rest_client_extension.rb
+++ lib/foreman/http_proxy/rest_client_extension.rb
@@ -3,10 +3,10 @@ RestClient::Request.class_eval do
   alias_method :orig_proxy_uri, :proxy_uri
 
   def proxy_uri
-    orig_proxy_uri
     if proxy_http_request?(@proxy, @uri.hostname, @uri.scheme)
       log_proxied_request(http_proxy, @uri.hostname)
       return URI.parse(http_proxy)
     end
+    orig_proxy_uri
   end
 end
