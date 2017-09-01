c9bb7a0c64e41869b5a4a2370e261c5ecc52ac5d
Sebastian Gräßl <mail@bastilian.me>
2017-09-01 10:59:59 +0200

    Filter inline notifications for regular flash notifications

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -516,9 +516,13 @@ module ApplicationHelper
   end
 
   def notifications
-    content_tag :div, :id => 'notifications', :'data-flash' => flash.to_json.html_safe do
+    content_tag :div, :id => 'notifications', :'data-flash' => flash_notifiations.to_json.html_safe do
       mount_react_component('ToastNotifications', '#notifications')
     end
+  end
+
+  def flash_notifiations
+    flash.select { |key, _| key != 'inline' }
   end
 
   def flash_inline
